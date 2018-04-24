7dc4be2c285e1074f32cd3b27cee37e79c1b16fa
Houdael <33830670+Houdael@users.noreply.github.com>
2018-04-24 12:10:08 +0100

    fxml updated, AppController class added

--- DBpediaQuiz/src/application/Main.java
+++ DBpediaQuiz/src/application/Main.java
@@ -5,25 +5,26 @@ import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
 
-public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		//try {
-		Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("App.fxml"));
-		Scene scene = new Scene(root);
-		//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-		primaryStage.setScene(scene);
-		primaryStage.setTitle("App");
-		primaryStage.show();
-	//} catch(Exception e) {
-		//e.printStackTrace();
-	//}
-	}
+
+public class App extends Application {
 	
 	public static void main(String[] args) {
 		launch(args);
 	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+			Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("App.fxml"));
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("App");
+			primaryStage.show();
+	
+	}
+	
+	
 }
