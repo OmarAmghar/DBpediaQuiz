6927d02300a5176b2ba25cef1fc28da8e9381836
Houdael <33830670+Houdael@users.noreply.github.com>
2018-04-25 18:13:32 +0100

    Start.fxml added, AppController class and application.css updated

--- DBpediaQuiz/src/application/AppController.java
+++ DBpediaQuiz/src/application/AppController.java
@@ -1,5 +1,25 @@
 package application;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class AppController {
 
+	public void startQuiz(ActionEvent e) throws Exception {
+		((Node)e.getSource()).getScene().getWindow().hide();
+		Stage primaryStage = new Stage();
+		Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Start.fxml"));
+		Scene scene = new Scene(root);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("StartMenu");
+		primaryStage.show();
+		
+	}
 }
